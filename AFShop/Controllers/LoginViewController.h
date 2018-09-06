50c3d6fab9983cc24bdd04ab2764e0f84ca5960f
xiaoyafei <471464321@qq.com>
2018-09-06 18:32:45 +0800

    代码结构

---
+++ AFShop/Controllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  AFShop
+//
+//  Created by 飞 on 2018/9/6.
+//  Copyright © 2018 飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ AFShop/Controllers/LoginViewController.m
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.m
+//  AFShop
+//
+//  Created by 飞 on 2018/9/6.
+//  Copyright © 2018 飞. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor blueColor];
+}
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
